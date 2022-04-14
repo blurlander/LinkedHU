4c4fb3e394a9be320a5068baea33c5e175931d48
bkisacs <44316961+b21727575@users.noreply.github.com>
2022-04-14 23:12:38 +0300

    Detailed version

---
+++ LinkedHU/src/main/java/DAO/GraduateDao.java
@@ -0,0 +1,40 @@
+package DAO;
+import java.util.List;
+
+import Interfaces.IDao;
+import Model.Graduate;
+
+public class GraduateDao implements IDao<Graduate>{
+
+	@Override
+	public List<Graduate> fetchAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(Graduate t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Graduate read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Graduate t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Graduate t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
---
+++ LinkedHU/src/main/java/DAO/MessageDao.java
@@ -0,0 +1,42 @@
+package DAO;
+import java.util.List;
+
+import Interfaces.IDao;
+import Model.Message;
+
+
+public class MessageDao implements IDao<Message>{
+
+	@Override
+	public List<Message> fetchAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(Message t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Message read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Message t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Message t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
